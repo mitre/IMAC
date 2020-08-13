3f86cd839ecfeeb3198596e64b1f327216ea934e
root <root@imac.mitre.org>
2020-08-13 18:24:57 -0400

    undo mongo package upgrades and document restore command

--- deploy/backup.sh
+++ deploy/backup.sh
@@ -6,3 +6,6 @@ find /opt/imac/backups/* -mtime +120 -exec rm {} \;
 # Backup mongodb with mongodump
 ARCHIVE_FILE=/opt/imac/backups/imac-mongodb-$(date --utc -Iminutes).archive.gzip
 mongodump --gzip --archive=$ARCHIVE_FILE
+
+# Restore using this command
+# mongorestore --gzip --archive=/path/to/archive.gzip
